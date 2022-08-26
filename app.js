85465c46122f24518faa3d62ac44b9e46e25d8e3
HadziqMasfuh <112155606+HadziqMasfuh@users.noreply.github.com>
2022-08-26 13:29:32 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,186 @@
+const nav = document.getElementById("navbar");
+const desc = document.getElementById("desc");
+const start = document.getElementById("start");
+const finish = document.getElementById("finish");
+
+desc.innerHTML = "hello";
+function out() {
+  nav.style.height = "0rem";
+}
+function p1() {
+  nav.style.height = "10rem";
+  desc.innerHTML = "Projek Jangka Panjang Hadziq";
+  start.innerHTML = new Date(2022, 6, 22).toDateString();
+  finish.innerHTML = "-";
+}
+function p1title() {
+  nav.style.height = "10rem";
+  desc.innerHTML = "Pembuatan Website Masjid Moed Har Arifin";
+  start.innerHTML = new Date(2022, 7, 20).toDateString();
+  finish.innerHTML = new Date(2022, 8, 30).toDateString();
+}
+function p1b1() {
+  nav.style.height = "10rem";
+  desc.innerHTML =
+    "Pembuatan Antar Muka Website Berpaku Pada Keindahan Design Dalam Manipulasi HTML dan CSS";
+  start.innerHTML = new Date(2022, 7, 20).toDateString();
+  finish.innerHTML = new Date(2022, 8, 30).toDateString();
+}
+function p1b2() {
+  nav.style.height = "10rem";
+  desc.innerHTML =
+    "Membuat Program Mesin Utama Pengelola Website Dimana Demua Data dan Integrasi Dibuat";
+  start.innerHTML = new Date(2022, 8, 5).toDateString();
+  finish.innerHTML = new Date(2022, 8, 30).toDateString();
+}
+function p1b1l1() {
+  nav.style.height = "10rem";
+  desc.innerHTML =
+    "Pembuatan desain utama konsep dari web, termasuk element, komposisi warna, bentuk, tampilan di device tertentu, animasi dan tulisan";
+  start.innerHTML = new Date(2022, 7, 20).toDateString();
+  finish.innerHTML = new Date(2022, 7, 31).toDateString();
+}
+function p1b1l2() {
+  nav.style.height = "10rem";
+  desc.innerHTML =
+    "implementasi design dengan javascript agar website resposive terhadap semua device (hp,pc,dll)";
+  start.innerHTML = new Date(2022, 7, 31).toDateString();
+  finish.innerHTML = new Date(2022, 8, 2).toDateString();
+}
+function p1b1l3() {
+  nav.style.height = "10rem";
+  desc.innerHTML = "Launching website ke internet sebagai prototype awal";
+  start.innerHTML = new Date(2022, 8, 2).toDateString();
+  finish.innerHTML = new Date(2022, 8, 3).toDateString();
+}
+function p1b1l4() {
+  nav.style.height = "10rem";
+  desc.innerHTML =
+    "pembuatan Web-applikasi untuk keperluan integrasi data masjid";
+  start.innerHTML = new Date(2022, 8, 3).toDateString();
+  finish.innerHTML = new Date(2022, 8, 30).toDateString();
+}
+function p1b2l1() {
+  nav.style.height = "10rem";
+  desc.innerHTML =
+    "Penyambungan Javascript dengan interface website untuk fungsi tombol, navbar dan lain-lain";
+  start.innerHTML = new Date(2022, 8, 1).toDateString();
+  finish.innerHTML = new Date(2022, 8, 3).toDateString();
+}
+function p1b2l2() {
+  nav.style.height = "10rem";
+  desc.innerHTML =
+    "pengambilan dan pemasukan data ke website melalui PHP dan MySQL";
+  start.innerHTML = new Date(2022, 8, 5).toDateString();
+  finish.innerHTML = new Date(2022, 8, 10).toDateString();
+}
+function p1b2l3() {
+  nav.style.height = "10rem";
+  desc.innerHTML =
+    "Testing ke device apakah webite sudah sepenuhnya terintegrasi dengan data";
+  start.innerHTML = new Date(2022, 8, 10).toDateString();
+  finish.innerHTML = new Date(2022, 8, 11).toDateString();
+}
+function p1b2l4() {
+  nav.style.height = "10rem";
+  desc.innerHTML =
+    "menkonversi HTML dan CSS ke format PHP untuk mempublish tema dan template sendiri agar bisa mengakses fitur fitur premium dari plugin wordpress";
+  start.innerHTML = new Date(2022, 8, 11).toDateString();
+  finish.innerHTML = new Date(2022, 8, 21).toDateString();
+}
+function p1b2l5() {
+  nav.style.height = "10rem";
+  desc.innerHTML =
+    "integrasi website dengan plugin wordpress agar bisa tersambung dengan media sosial yang ada dan keperluan integrasi lainnya menggunakan plugin yang ada atau membuat plugin sendiri bila diperlukan";
+  start.innerHTML = new Date(2022, 8, 21).toDateString();
+  finish.innerHTML = new Date(2022, 8, 28).toDateString();
+}
+function p1b2l6() {
+  nav.style.height = "10rem";
+  desc.innerHTML = "mumpublish website ke hostinger wordpress";
+  start.innerHTML = new Date(2022, 8, 28).toDateString();
+  finish.innerHTML = new Date(2022, 8, 30).toDateString();
+}
+function p2title() {
+  nav.style.height = "10rem";
+  desc.innerHTML =
+    "Pembuatan server untuk mempermudah penanganan informasi yang masuk dan keluar";
+  start.innerHTML = new Date(2022, 8, 30).toDateString();
+  finish.innerHTML = new Date(2022, 9, 30).toDateString();
+}
+function p2b1() {
+  nav.style.height = "10rem";
+  desc.innerHTML =
+    "pembuatan database sebagai sumber utama penyimpanan informasi dan alamat informasi";
+  start.innerHTML = new Date(2022, 8, 30).toDateString();
+  finish.innerHTML = new Date(2022, 9, 10).toDateString();
+}
+function p2b2() {
+  nav.style.height = "10rem";
+  desc.innerHTML = "pengintegrasi database yang ada dan pembuatan applikasinya";
+  start.innerHTML = new Date(2022, 9, 10).toDateString();
+  finish.innerHTML = new Date(2022, 9, 30).toDateString();
+}
+function p2b1l1() {
+  nav.style.height = "10rem";
+  desc.innerHTML =
+    "Membuat Sistem VPS (Virtual Private Server) menngunakan OS Linux Debian/RaspBian menngunakan device  minimum Raspberry PI type 4B atau diatasnya";
+  start.innerHTML = new Date(2022, 8, 30).toDateString();
+  finish.innerHTML = new Date(2022, 9, 3).toDateString();
+}
+function p2b1l2() {
+  nav.style.height = "10rem";
+  desc.innerHTML =
+    "Pembuatan Database Berdasar pada struktur data PSQL dikarenakan PSQL merupakan database Cepat yang mensupport banyak jenis data dibanding database lain";
+  start.innerHTML = new Date(2022, 9, 3).toDateString();
+  finish.innerHTML = new Date(2022, 9, 6).toDateString();
+}
+function p2b1l3() {
+  nav.style.height = "10rem";
+  desc.innerHTML =
+    "memasukkan data yang sudah ada ke tabel database pada PSQL dan membuat constrain untuk mengikat data yang berhubungan";
+  start.innerHTML = new Date(2022, 9, 6).toDateString();
+  finish.innerHTML = new Date(2022, 9, 9).toDateString();
+}
+function p2b1l4() {
+  nav.style.height = "10rem";
+  desc.innerHTML =
+    "membuka port di server sehingga bisa diakses online sekaligus pengujian";
+  start.innerHTML = new Date(2022, 9, 9).toDateString();
+  finish.innerHTML = new Date(2022, 9, 10).toDateString();
+}
+function p2b1l5() {
+  nav.style.height = "10rem";
+  desc.innerHTML =
+    "pembuatan applikasi baik android,windows dll untuk keperluan kemudahan integrasi data ke server database";
+  start.innerHTML = new Date(2022, 9, 10).toDateString();
+  finish.innerHTML = new Date(2022, 9, 30).toDateString();
+}
+function p2b2l1() {
+  nav.style.height = "10rem";
+  desc.innerHTML =
+    "Request dari mas Syamsu agar data vidio dan gambar bisa terintegrasi untuk mempercepat kinerja designer dalam menjalankan tugas";
+  start.innerHTML = new Date(2022, 9, 10).toDateString();
+  finish.innerHTML = new Date(2022, 9, 13).toDateString();
+}
+function p2b2l2() {
+  nav.style.height = "10rem";
+  desc.innerHTML =
+    "Request dari pak dharmo agar data dari jamaah masjid bisa dikelola dengan mudah";
+  start.innerHTML = new Date(2022, 9, 13).toDateString();
+  finish.innerHTML = new Date(2022, 9, 21).toDateString();
+}
+function p2b2l3() {
+  nav.style.height = "10rem";
+  desc.innerHTML =
+    "Request dari pak dharmo untuk mempermudah dalam penyebaran informasi via whats-app dan mempermudah pekerjaan customer sevice";
+  start.innerHTML = new Date(2022, 9, 21).toDateString();
+  finish.innerHTML = new Date(2022, 9, 30).toDateString();
+}
+function p2b2l4() {
+  nav.style.height = "10rem";
+  desc.innerHTML =
+    "beberapa data akan dibuat transparan dan ditunjukkan ke halaman utama web masjid sebagai sumber penyebaran informasi pusat";
+  start.innerHTML = new Date(2022, 9, 30).toDateString();
+  finish.innerHTML = new Date(2022, 10, 1).toDateString();
+}
